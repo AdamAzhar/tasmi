24d47b9f5d417a1961cfdbc3ea2fc88ddfeb706a
Adam_Azhar <adamazhar401@gmail.com>
2024-11-16 11:10:21 +0700

    Kelompok adam

---
+++ app/Http/Controllers/SantriController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Santri;
+use App\Models\Siswa;
+use Illuminate\Http\Request;
+
+class SantriController extends Controller
+{
+    // public function index();
+    // // $data = Santri::all();
+    
+}
